0652e657b9dc31654c3cde86d7a35cd4fdc77a88
Bray <bray@coreforge.com>
2019-01-26 17:28:10 -0800

    Added code and preview

---
+++ Arduino-DeDe-tester.ino
@@ -0,0 +1,60 @@
+/*
+ * Test sketch for the Arduino Debugger & Development Shield - DeDe
+ * Bray Almini 2019.01.26
+ *
+ * Cycle through all the pins in both digital and analog.
+ *
+ * https://github.com/Brayyy/Arduino-DeDe-tester
+ * https://www.kickstarter.com/projects/ellutions/dede-debug-and-development-shield-for-arduinos-and
+ */
+
+int timer1 = 150;
+int timer2 = 50;
+int myPins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3, A4, A5};
+int myPinCount = 20;
+int toggleState = 0;
+
+void setup() {
+  // use a for loop to initialize each pin as an output:
+  for (int count=0; count < myPinCount; count++) {
+    pinMode(myPins[count], OUTPUT);
+  }
+}
+
+void loop() {
+
+  delay(5000);
+
+
+  // Toggle digital HIGH, then LOW
+  for (int j = 0; j < 2; j++) {
+    // Flip the toggle state
+    toggleState = !toggleState;
+
+    // loop from the lowest pin to the highest:
+    for (int i = 0; i < myPinCount; i++) {
+      digitalWrite(myPins[i], toggleState);
+      delay(timer1);
+    }
+  }
+
+  delay(1000);
+
+  // Fade analog 0-255 across all pins. Pins not supporting analog PWM output will go directly HIGH
+  // Loop from the lowest pin to the highest:
+  for (int i = 0; i < myPinCount; i++) {
+    for (int brightness = 0; brightness <= 255; brightness += 15) {
+      analogWrite(myPins[i], brightness);
+      delay(timer2);
+    }
+  }
+
+  delay(1000);
+
+  // Return all pins to LOW
+  for (int i = 0; i < myPinCount; i++) {
+    digitalWrite(myPins[i], 0);
+  }
+
+  delay(1000);
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Arduino-DeDe-tester
+
+This is a small script to test the outputs of the Arduino DeDe (Debugger & Development) Shield, cycling through all the pins in both digital and analog modes.
+
+After my DeDe arrived in the mail, I wanted a quick script to run through all the I/O, and make sure all lights work as expected.
+
+![](preview.gif)
